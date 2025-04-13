e72858f342758aa49fb4fc2454f2113e09480a8b
MayankSinghRathode01 <mayankrathode01@gmail.com>
2025-04-13 15:24:27 +0530

    Generate reply API

---
+++ src/test/java/com/email/email_assistant_sb/EmailAssistantSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.email_assistant_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAssistantSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
